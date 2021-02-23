bfe1bf75cd49f3123e95a5c4c1d8bde2a3001207
Mike Gavaghan <mike@gavaghan.org>
2021-02-23 15:47:24 -0600

    Doc updates

--- src/main/java/org/gavaghan/json/JSONNumber.java
+++ src/main/java/org/gavaghan/json/JSONNumber.java
@@ -27,189 +27,187 @@ import java.math.BigDecimal;
  */
 public class JSONNumber extends AbstractJSONValue
 {
-	/** The underlying value. **/
-	private BigDecimal mValue;
+   /** The underlying value. **/
+   private BigDecimal mValue;
 
-	/**
-	 * Read the whole portion of a number.
-	 * 
-	 * @param pbr
-	 * @param builder
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	private void readWholePart(PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
-	{
-		char c;
-		for (;;)
-		{
-			c = JSONValueFactory.demand(pbr);
-			if (Character.isDigit(c))
-			{
-				builder.append(c);
-			}
-			else
-			{
-				pbr.unread(c);
-				break;
-			}
-		}
-	}
+   /**
+    * Read the whole portion of a number.
+    * 
+    * @param pbr
+    * @param builder
+    * @throws IOException
+    * @throws JSONException
+    */
+   private void readWholePart(PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
+   {
+      char c;
+      for (;;)
+      {
+         c = JSONValueFactory.demand(pbr);
+         if (Character.isDigit(c))
+         {
+            builder.append(c);
+         }
+         else
+         {
+            pbr.unread(c);
+            break;
+         }
+      }
+   }
 
-	/**
-	 * Read the fractional part of the number.
-	 * 
-	 * @param path
-	 *           path to the value being read
-	 * @param pbr
-	 * @param builder
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	private void readFractionalPart(String path, PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
-	{
-		char c;
-		c = JSONValueFactory.demand(pbr);
-		if (c == '.')
-		{
-			builder.append(c);
+   /**
+    * Read the fractional part of the number.
+    * 
+    * @param path    path to the value being read
+    * @param pbr
+    * @param builder
+    * @throws IOException
+    * @throws JSONException
+    */
+   private void readFractionalPart(String path, PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
+   {
+      char c;
+      c = JSONValueFactory.demand(pbr);
+      if (c == '.')
+      {
+         builder.append(c);
 
-			for (;;)
-			{
-				c = JSONValueFactory.demand(pbr);
-				if (!Character.isDigit(c))
-				{
-					if (builder.toString().endsWith(".")) throw new JSONException(path, "Digits expected after decimal points.");
-					pbr.unread(c);
-					break;
-				}
+         for (;;)
+         {
+            c = JSONValueFactory.demand(pbr);
+            if (!Character.isDigit(c))
+            {
+               if (builder.toString().endsWith(".")) throw new JSONException(path, "Digits expected after decimal points.");
+               pbr.unread(c);
+               break;
+            }
 
-				builder.append(c);
-			}
-		}
-		else
-		{
-			pbr.unread(c);
-		}
-	}
+            builder.append(c);
+         }
+      }
+      else
+      {
+         pbr.unread(c);
+      }
+   }
 
-	/**
-	 * Read the exponent.
-	 * 
-	 * @param path
-	 *           path to the value being read
-	 * @param pbr
-	 * @param builder
-	 * @throws IOException
-	 * @throws JSONException
-	 */
-	private void readExponent(String path, PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
-	{
-		char c;
-		c = JSONValueFactory.demand(pbr);
-		if (c == 'e' || (c == 'E'))
-		{
-			builder.append(c);
+   /**
+    * Read the exponent.
+    * 
+    * @param path    path to the value being read
+    * @param pbr
+    * @param builder
+    * @throws IOException
+    * @throws JSONException
+    */
+   private void readExponent(String path, PushbackReader pbr, StringBuilder builder) throws IOException, JSONException
+   {
+      char c;
+      c = JSONValueFactory.demand(pbr);
+      if (c == 'e' || (c == 'E'))
+      {
+         builder.append(c);
 
-			c = JSONValueFactory.demand(pbr);
+         c = JSONValueFactory.demand(pbr);
 
-			if (Character.isDigit(c) || (c == '+') || (c == '-'))
-			{
-				builder.append(c);
+         if (Character.isDigit(c) || (c == '+') || (c == '-'))
+         {
+            builder.append(c);
 
-				for (;;)
-				{
-					c = JSONValueFactory.demand(pbr);
-					if (!Character.isDigit(c))
-					{
-						pbr.unread(c);
-						break;
-					}
+            for (;;)
+            {
+               c = JSONValueFactory.demand(pbr);
+               if (!Character.isDigit(c))
+               {
+                  pbr.unread(c);
+                  break;
+               }
 
-					builder.append(c);
-				}
-			}
-			else throw new JSONException(path, "Content does not appear to be a number");
-		}
-		else
-		{
-			pbr.unread(c);
-		}
-	}
+               builder.append(c);
+            }
+         }
+         else throw new JSONException(path, "Content does not appear to be a number");
+      }
+      else
+      {
+         pbr.unread(c);
+      }
+   }
 
-	/**
-	 * Create a new JSONNumber.
-	 * 
-	 * @param value
-	 */
-	public JSONNumber(BigDecimal value)
-	{
-		if (value == null) throw new NullPointerException("Null value not allowed.  Use JSONNull instead.");
-		mValue = value;
-	}
+   /**
+    * Create a new JSONNumber.
+    * 
+    * @param value
+    */
+   public JSONNumber(BigDecimal value)
+   {
+      if (value == null) throw new NullPointerException("Null value not allowed.  Use JSONNull instead.");
+      mValue = value;
+   }
 
-	/**
-	 * Create a new JSONNumber.
-	 * 
-	 * @param value
-	 */
-	public JSONNumber(long value)
-	{
-		mValue = new BigDecimal(value);
-	}
+   /**
+    * Create a new JSONNumber.
+    * 
+    * @param value
+    */
+   public JSONNumber(long value)
+   {
+      mValue = new BigDecimal(value);
+   }
 
-	/**
-	 * Create a new JSONNumber.
-	 * 
-	 * @param value
-	 */
-	public JSONNumber(double value)
-	{
-		mValue = new BigDecimal(value);
-	}
+   /**
+    * Create a new JSONNumber.
+    * 
+    * @param value
+    */
+   public JSONNumber(double value)
+   {
+      mValue = new BigDecimal(value);
+   }
 
-	/**
-	 * Create a new JSONNumber.
-	 * 
-	 * @param value
-	 */
-	public JSONNumber(String value)
-	{
-		mValue = new BigDecimal(value);
-	}
+   /**
+    * Create a new JSONNumber.
+    * 
+    * @param value
+    */
+   public JSONNumber(String value)
+   {
+      mValue = new BigDecimal(value);
+   }
 
-	/**
-	 * Create a new JSONNumber.
-	 */
-	public JSONNumber()
-	{
-		mValue = BigDecimal.ZERO;
-	}
+   /**
+    * Create a new JSONNumber.
+    */
+   public JSONNumber()
+   {
+      mValue = BigDecimal.ZERO;
+   }
 
-	/**
-	 * Set the underlying value.
-	 * 
-	 * @param value
-	 */
-	public void setValue(BigDecimal value)
-	{
-		if (value == null) throw new NullPointerException("Null value not allowed.  Use JSONNull instead.");
-		mValue = value;
-	}
-
-	/**
-	 * Get the underlying value.
-	 * 
-	 * @return
-	 */
-	@Override
-	public Object getValue()
-	{
-		return mValue;
-	}
+   /**
+    * Set the underlying value.
+    * 
+    * @param value
+    */
+   public void setValue(BigDecimal value)
+   {
+      if (value == null) throw new NullPointerException("Null value not allowed.  Use JSONNull instead.");
+      mValue = value;
+   }
 
    /**
     * Get the underlying value.
+    * 
+    * @return
+    */
+   @Override
+   public Object getValue()
+   {
+      return mValue;
+   }
+
+   /**
+    * Get the underlying value as a <code>BigDecimal</code>.
     * 
     * @since 1.2
     * 
@@ -220,84 +218,77 @@ public class JSONNumber extends AbstractJSONValue
       return mValue;
    }
 
-	/**
-	 * Create a prototype instance of the same type.
-	 * 
-	 * @return
-	 */
-	@Override
-	public JSONValue createPrototype()
-	{
-		return new JSONNumber();
-	}
+   /**
+    * Create a prototype instance of the same type.
+    * 
+    * @return
+    */
+   @Override
+   public JSONValue createPrototype()
+   {
+      return new JSONNumber();
+   }
 
-	/**
-	 * Copy the value of another JSONValue into our underlying value.
-	 * 
-	 * @param value
-	 */
-	@Override
-	public void copyValue(JSONValue value)
-	{
-		if (!getClass().isAssignableFrom(value.getClass())) throw new RuntimeException("Can't assign a " + value.getClass().getName() + " to a " + getClass().getName());
+   /**
+    * Copy the value of another JSONValue into our underlying value.
+    * 
+    * @param value
+    */
+   @Override
+   public void copyValue(JSONValue value)
+   {
+      if (!getClass().isAssignableFrom(value.getClass())) throw new RuntimeException("Can't assign a " + value.getClass().getName() + " to a " + getClass().getName());
 
-		mValue = (BigDecimal) value.getValue();
-	}
+      mValue = (BigDecimal) value.getValue();
+   }
 
-	/**
-	 * Read a JSON value (presumes the key has already been read) and set the
-	 * underlying value. There's generally no reason to call this method
-	 * directly. It is intended to be overridden by an extended type.
-	 * 
-	 * @param path
-	 *           path to the value being read
-	 * @param pbr
-	 *           source reader
-	 * @throws IOException
-	 *            on read failure
-	 * @throws JSONException
-	 *            on grammar error
-	 */
-	@Override
-	public void read(String path, PushbackReader pbr) throws IOException, JSONException
-	{
-		StringBuilder builder = new StringBuilder();
+   /**
+    * Read a JSON value (presumes the key has already been read) and set the
+    * underlying value. There's generally no reason to call this method directly.
+    * It is intended to be overridden by an extended type.
+    * 
+    * @param path path to the value being read
+    * @param pbr  source reader
+    * @throws IOException   on read failure
+    * @throws JSONException on grammar error
+    */
+   @Override
+   public void read(String path, PushbackReader pbr) throws IOException, JSONException
+   {
+      StringBuilder builder = new StringBuilder();
 
-		char c = JSONValueFactory.demand(pbr);
-		if (!Character.isDigit(c) && (c != '-')) throw new JSONException(path, "Content does not appear to be a number.");
+      char c = JSONValueFactory.demand(pbr);
+      if (!Character.isDigit(c) && (c != '-')) throw new JSONException(path, "Content does not appear to be a number.");
 
-		builder.append(c);
+      builder.append(c);
 
-		// read the number
-		if (c != '0') readWholePart(pbr, builder);
-		readFractionalPart(path, pbr, builder);
-		readExponent(path, pbr, builder);
+      // read the number
+      if (c != '0') readWholePart(pbr, builder);
+      readFractionalPart(path, pbr, builder);
+      readExponent(path, pbr, builder);
 
-		// parse and set value
-		try
-		{
-			mValue = new BigDecimal(builder.toString());
-		}
-		catch (NumberFormatException exc)
-		{
-			throw new JSONException(path, "Illegal number format: " + builder.toString());
-		}
-	}
+      // parse and set value
+      try
+      {
+         mValue = new BigDecimal(builder.toString());
+      }
+      catch (NumberFormatException exc)
+      {
+         throw new JSONException(path, "Illegal number format: " + builder.toString());
+      }
+   }
 
-	/**
-	 * Render this JSON value to a Writer.
-	 * 
-	 * @param indent
-	 *           indent padding
-	 * @param writer
-	 *           target writer
-	 * @param pretty
-	 *           'true' for pretty-print, 'false' for flat
-	 * @throws IOException
-	 */
-	@Override
-	public void write(String indent, Writer writer, boolean pretty) throws IOException
-	{
-		writer.write(mValue.toString());
-	}
+   /**
+    * Render this JSON value to a Writer.
+    * 
+    * @param indent indent padding
+    * @param writer target writer
+    * @param pretty 'true' for pretty-print, 'false' for flat
+    * @throws IOException
+    */
+   @Override
+   public void write(String indent, Writer writer, boolean pretty) throws IOException
+   {
+      writer.write(mValue.toString());
+   }
 }
--- src/main/java/org/gavaghan/json/JSONObject.java
+++ src/main/java/org/gavaghan/json/JSONObject.java
@@ -21,52 +21,52 @@ import java.io.Writer;
 import java.util.LinkedHashMap;
 
 /**
- * A JSON object as defined by <a href="http://www.json.org/">http://www.json.org/</a>
+ * A JSON object as defined by
+ * <a href="http://www.json.org/">http://www.json.org/</a>
  * 
  * @author <a href="mailto:mike@gavaghan.org">Mike Gavaghan</a>
  */
 public class JSONObject extends LinkedHashMap<String, JSONValue> implements JSONValue
 {
-	/**
-	 * System dependent end-of-line delimiter take from system property
-	 * "line.separator"
-	 */
-	static public final String EOL = System.getProperty("line.separator");
+   /**
+    * System dependent end-of-line delimiter take from system property
+    * "line.separator"
+    */
+   static public final String EOL = System.getProperty("line.separator");
 
-	/** JSONValueFactory for reading from a Reader. */
-	private JSONValueFactory mFactory;
-
-	/**
-	 * Create a new JSONObject.
-	 * 
-	 * @param factory
-	 *           the factory implementation used to read values in the object.
-	 */
-	protected JSONObject(JSONValueFactory factory)
-	{
-		mFactory = factory;
-	}
-
-	/**
-	 * Create a new JSONObject.
-	 */
-	public JSONObject()
-	{
-	}
-
-	/**
-	 * Get the underlying value.
-	 * 
-	 * @return
-	 */
-	@Override
-	public Object getValue()
-	{
-		return this;
-	}
+   /** JSONValueFactory for reading from a Reader. */
+   private JSONValueFactory mFactory;
 
    /**
-    * Get the underlying value as a JSONObject.
+    * Create a new JSONObject.
+    * 
+    * @param factory the factory implementation used to read values in the object.
+    */
+   protected JSONObject(JSONValueFactory factory)
+   {
+      mFactory = factory;
+   }
+
+   /**
+    * Create a new JSONObject.
+    */
+   public JSONObject()
+   {
+   }
+
+   /**
+    * Get the underlying value.
+    * 
+    * @return
+    */
+   @Override
+   public Object getValue()
+   {
+      return this;
+   }
+
+   /**
+    * Get the underlying value as a <code>JSONObject</code>.
     * 
     * @since 1.2
     * 
@@ -77,197 +77,190 @@ public class JSONObject extends LinkedHashMap<String, JSONValue> implements JSON
       return this;
    }
 
-	/**
-	 * Create a prototype instance of the same type.
-	 * 
-	 * @return
-	 */
-	@Override
-	public JSONValue createPrototype()
-	{
-		return new JSONObject();
-	}
+   /**
+    * Create a prototype instance of the same type.
+    * 
+    * @return
+    */
+   @Override
+   public JSONValue createPrototype()
+   {
+      return new JSONObject();
+   }
 
-	/**
-	 * Copy the value of another JSONValue into our underlying value.
-	 * 
-	 * @param value
-	 */
-	@Override
-	public void copyValue(JSONValue value)
-	{
-		if (!JSONObject.class.isAssignableFrom(value.getClass())) throw new RuntimeException("Can't assign a " + value.getClass().getName() + " to a " + getClass().getName());
+   /**
+    * Copy the value of another JSONValue into our underlying value.
+    * 
+    * @param value
+    */
+   @Override
+   public void copyValue(JSONValue value)
+   {
+      if (!JSONObject.class.isAssignableFrom(value.getClass())) throw new RuntimeException("Can't assign a " + value.getClass().getName() + " to a " + getClass().getName());
 
-      JSONObject source = (JSONObject)value.getValue();
+      JSONObject source = (JSONObject) value.getValue();
 
       for (String key : source.keySet())
       {
-        put(key, source.get(key));
+         put(key, source.get(key));
       }
-	}
+   }
 
-	/**
-	 * Create a deep copy of this instance.
-	 * 
-	 * @return
-	 */
-	@Override
-	public JSONValue deepCopy()
-	{
+   /**
+    * Create a deep copy of this instance.
+    * 
+    * @return
+    */
+   @Override
+   public JSONValue deepCopy()
+   {
       JSONValue copy = createPrototype();
       copy.copyValue(this);
       return copy;
 
-	}
+   }
 
-	/**
-	 * Read a JSON value (presumes the key has already been read) and set the
-	 * underlying value. There's generally no reason to call this method
-	 * directly. It is intended to be overridden by an extended type.
-	 * 
-	 * @param path
-	 *           path to the value being read
-	 * @param pbr
-	 *           source reader
-	 * @throws IOException
-	 *            on read failure
-	 * @throws JSONException
-	 *            on grammar error
-	 */
-	@Override
-	public void read(String path, PushbackReader pbr) throws IOException, JSONException
-	{
-		// assert we have an opening brace
-		char c = JSONValueFactory.demand(pbr);
-		if (c != '{') throw new JSONException(path, "Failed to find '{' at start of JSON object.");
+   /**
+    * Read a JSON value (presumes the key has already been read) and set the
+    * underlying value. There's generally no reason to call this method directly.
+    * It is intended to be overridden by an extended type.
+    * 
+    * @param path path to the value being read
+    * @param pbr  source reader
+    * @throws IOException   on read failure
+    * @throws JSONException on grammar error
+    */
+   @Override
+   public void read(String path, PushbackReader pbr) throws IOException, JSONException
+   {
+      // assert we have an opening brace
+      char c = JSONValueFactory.demand(pbr);
+      if (c != '{') throw new JSONException(path, "Failed to find '{' at start of JSON object.");
 
-		for (;;)
-		{
-			String key;
+      for (;;)
+      {
+         String key;
 
-			// next is either a key or a closing brace
-			mFactory.skipWhitespace(pbr);
-			c = JSONValueFactory.demand(pbr);
+         // next is either a key or a closing brace
+         mFactory.skipWhitespace(pbr);
+         c = JSONValueFactory.demand(pbr);
 
-			// is it a string?
-			if (c == '\"')
-			{
-				pbr.unread(c);
-				key = JSONString.readString(path, pbr);
-			}
-			// is it a closing brace?
-			else if (c == '}')
-			{
-				break;
-			}
-			// else, it's poorly formed
-			else
-			{
-				throw new JSONException(path, "JSON object is not grammatically correct.  Unexpected: " + c);
-			}
+         // is it a string?
+         if (c == '\"')
+         {
+            pbr.unread(c);
+            key = JSONString.readString(path, pbr);
+         }
+         // is it a closing brace?
+         else if (c == '}')
+         {
+            break;
+         }
+         // else, it's poorly formed
+         else
+         {
+            throw new JSONException(path, "JSON object is not grammatically correct.  Unexpected: " + c);
+         }
 
-			// next ought to be a colon
-			mFactory.skipWhitespace(pbr);
-			c = JSONValueFactory.demand(pbr);
-			if (c != ':') throw new JSONException(path + "." + key, "Expected ':' after key value");
-			mFactory.skipWhitespace(pbr);
+         // next ought to be a colon
+         mFactory.skipWhitespace(pbr);
+         c = JSONValueFactory.demand(pbr);
+         if (c != ':') throw new JSONException(path + "." + key, "Expected ':' after key value");
+         mFactory.skipWhitespace(pbr);
 
-			// next, read a JSONValue
-			JSONValue value = mFactory.read(path + "." + key, pbr);
+         // next, read a JSONValue
+         JSONValue value = mFactory.read(path + "." + key, pbr);
 
-			// add it to the map
-			put(key, value);
+         // add it to the map
+         put(key, value);
 
-			// next must be comma or close
-			mFactory.skipWhitespace(pbr);
-			c = JSONValueFactory.demand(pbr);
+         // next must be comma or close
+         mFactory.skipWhitespace(pbr);
+         c = JSONValueFactory.demand(pbr);
 
-			if (c == ',') continue;
-			if (c == '}') break;
+         if (c == ',') continue;
+         if (c == '}') break;
 
-			throw new JSONException(path, "JSON object is not grammatically correct.  Unexpected: " + c);
-		}
-	}
+         throw new JSONException(path, "JSON object is not grammatically correct.  Unexpected: " + c);
+      }
+   }
 
-	/**
-	 * Render this JSON value to a Writer.
-	 * 
-	 * @param indent
-	 *           indent padding
-	 * @param writer
-	 *           target writer
-	 * @param pretty
-	 *           'true' for pretty-print, 'false' for flat
-	 * @throws IOException
-	 */
-	@Override
-	public void write(String indent, Writer writer, boolean pretty) throws IOException
-	{
-		String newIndent = indent + "   ";
-		
-		// if JSONObject has been subtyped, included the class name in the JSON
-		if (this instanceof TypedJSONObject)
-		{
-		   ((TypedJSONObject) this).getType();
-		}
+   /**
+    * Render this JSON value to a Writer.
+    * 
+    * @param indent indent padding
+    * @param writer target writer
+    * @param pretty 'true' for pretty-print, 'false' for flat
+    * @throws IOException
+    */
+   @Override
+   public void write(String indent, Writer writer, boolean pretty) throws IOException
+   {
+      String newIndent = indent + "   ";
 
-		if (size() == 0)
-		{
-			writer.write("{}");
-		}
-		else
-		{
-			writer.write('{');
+      // if JSONObject has been subtyped, included the class name in the JSON
+      if (this instanceof TypedJSONObject)
+      {
+         ((TypedJSONObject) this).getType();
+      }
 
-			int count = 1;
+      if (size() == 0)
+      {
+         writer.write("{}");
+      }
+      else
+      {
+         writer.write('{');
 
-			if (pretty) writer.write(EOL);
+         int count = 1;
 
-			for (String key : keySet())
-			{
-				if (pretty) writer.write(newIndent);
-				writer.write('\"');
-				writer.write(key);
-				writer.write("\":");
-				if (pretty) writer.write(" ");
+         if (pretty) writer.write(EOL);
 
-				get(key).write(newIndent, writer, pretty);
+         for (String key : keySet())
+         {
+            if (pretty) writer.write(newIndent);
+            writer.write('\"');
+            writer.write(key);
+            writer.write("\":");
+            if (pretty) writer.write(" ");
 
-				if (count != size()) writer.write(',');
+            get(key).write(newIndent, writer, pretty);
 
-				if (pretty) writer.write(EOL);
-				count++;
-			}
+            if (count != size()) writer.write(',');
 
-			writer.write(indent);
-			writer.write('}');
-		}
-	}
+            if (pretty) writer.write(EOL);
+            count++;
+         }
 
-	/**
-	 * Render this object as a string.
-	 */
-	@Override
-	public String toString()
-	{
-		return toPrettyString();
-	}
+         writer.write(indent);
+         writer.write('}');
+      }
+   }
 
-	/**
-	 * Render this object as a pretty-printed string.
-	 */
-	@Override
-	public String toPrettyString()
-	{
-		return AbstractJSONValue.toString(this, true);
-	}
+   /**
+    * Render this object as a string.
+    */
+   @Override
+   public String toString()
+   {
+      return toPrettyString();
+   }
 
-	/**
-	 * Render this object as a flattened string.
-	 */
-	@Override
-	public String toFlatString()
-	{
-		return AbstractJSONValue.toString(this, false);
-	}
+   /**
+    * Render this object as a pretty-printed string.
+    */
+   @Override
+   public String toPrettyString()
+   {
+      return AbstractJSONValue.toString(this, true);
+   }
+
+   /**
+    * Render this object as a flattened string.
+    */
+   @Override
+   public String toFlatString()
+   {
+      return AbstractJSONValue.toString(this, false);
+   }
 }
